038ad7527d7a9ac91e8ed02dc4bc43572d9c9a2e
Jason Ogbeide <92401055+jasonogbeide@users.noreply.github.com>
2025-07-15 07:58:48 +0000

    Update IPs that can access

--- sg.tf
+++ sg.tf
@@ -8,13 +8,13 @@ resource "null_resource" "example" {
         from_port   = 22
         to_port     = 22
         protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = ["10.0.0.0/16"]
       }]
       egress = [{
         from_port   = 0
         to_port     = 0
         protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = ["10.0.0.0/16"]
       }]
     })
   }
